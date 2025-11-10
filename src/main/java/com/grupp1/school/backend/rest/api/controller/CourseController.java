6dd42306b17c50c0346d9b707af967945f8bf7e1
Jonathan Rubensson <jonathan@eldfluga.se>
2025-11-10 16:37:11 +0100

    Add remaining crud for course controller

--- src/main/java/com/grupp1/school/backend/rest/api/controller/CourseController.java
+++ src/main/java/com/grupp1/school/backend/rest/api/controller/CourseController.java
@@ -2,13 +2,11 @@ package com.grupp1.school.backend.rest.api.controller;
 
 import com.grupp1.school.backend.rest.api.model.dto.CourseDTO;
 import com.grupp1.school.backend.rest.api.service.CourseService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Positive;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,7 +25,25 @@ public class CourseController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CourseDTO> getCourseById(@Min(value = 1, message = "Bad Request") @PathVariable Integer id){
+    public ResponseEntity<CourseDTO> getCourseById(@Min(value = 1, message = "ID needs to be non-zero positive integer.") @PathVariable Integer id){
         return service.getById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> addCourse(@Valid @RequestBody CourseDTO dto){
+        return ResponseEntity.ok(service.create(dto));
+    }
+
+    @PutMapping
+    public ResponseEntity<CourseDTO> updateCourse(@Valid @RequestBody CourseDTO dto) {
+        return service.update(dto).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCourse(@Min(value = 1, message = "ID needs to be non-zero positive integer.") @PathVariable Integer id){
+        if (service.deleteById(id)){
+            return ResponseEntity.ok("Deletion successful.");
+        }
+        return ResponseEntity.notFound().build();
     }
 }
