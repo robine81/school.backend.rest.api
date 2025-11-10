eb9e93a3cf71fdf6ca288fad49df5c6b78495f4e
robine81 <robine.westberg@gmail.com>
2025-11-10 12:09:47 +0100

    Student structure

--- src/main/java/com/grupp1/school/backend/rest/api/model/Student.java
+++ src/main/java/com/grupp1/school/backend/rest/api/model/Student.java
@@ -1,4 +1,33 @@
 package com.grupp1.school.backend.rest.api.model;
 
 public class Student {
+    private int studentId;
+    private String studentName;
+    private int studentAge;
+    private String studentEmail;
+
+    public Student(int studentId, String studentName, int studentAge, String studentEmail) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentAge = studentAge;
+        this.studentEmail = studentEmail;
+    }
+
+    public Student() {}
+
+    public int getStudentId() { return studentId; }
+
+    public void setStudentId(int studentId) { this.studentId = studentId; }
+
+    public String getStudentName() { return studentName; }
+
+    public void setStudentName(String studentName) { this.studentName = studentName; }
+
+    public int getStudentAge() { return studentAge; }
+
+    public void setStudentAge(int studentAge) { this.studentAge = studentAge; }
+
+    public String getStudentEmail() { return studentEmail; }
+
+    public void setStudentEmail(String studentEmail) { this.studentEmail = studentEmail; }
 }
--- src/main/java/com/grupp1/school/backend/rest/api/repository/StudentRepository.java
+++ src/main/java/com/grupp1/school/backend/rest/api/repository/StudentRepository.java
@@ -1,4 +1,39 @@
 package com.grupp1.school.backend.rest.api.repository;
 
+import com.grupp1.school.backend.rest.api.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class StudentRepository {
+    private List<Student> students = new ArrayList<>();
+
+    public List<Student> findAll(){ return students; }
+
+    public Optional<Student> findById(int id){
+        return students.stream()
+                .filter(s -> s.getStudentId() == id)
+                .findFirst();
+    }
+
+    public List<Student> findByName(String name) {
+        return students.stream()
+                .filter(s -> s.getStudentName()
+                        .toLowerCase()
+                        .contains(name.toLowerCase()))
+                .toList();
+    }
+
+    public Student save(Student student){
+        int nextId = students.stream()
+                .mapToInt(s -> s.getStudentId())
+                .max()
+                .orElse(0) +1;
+        student.setStudentId(nextId);
+        students.add(student);
+        return student;
+    }
+
+    public boolean deleteById(int id) { return students.removeIf(s -> s.getStudentId() == id); }
 }
