439c1c159185e053fc94dbdeb36a116ebe75d004
robine81 <robine.westberg@gmail.com>
2025-11-10 11:00:18 +0100

    Initial commit

---
+++ src/main/java/com/grupp1/school/backend/rest/api/Application.java
@@ -0,0 +1,13 @@
+package com.grupp1.school.backend.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupp1/school/backend/rest/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp1.school.backend.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
